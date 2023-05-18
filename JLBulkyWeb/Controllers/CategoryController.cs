16cd0ea5c8f84ceb0a8099cf2e0694797776b29a
daily-dev-jake <jlele92@outlook.com>
2023-05-18 23:58:34 +0800

    - Connected DB - Created CategoryController - Created Category View - Display Category View on Navbar - Seeded Category Model into DB with 3 items - Retrieved Category Model and display to the Category tab in a tabular form

---
+++ JLBulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using JLBulkyWeb.Data;
+using JLBulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JLBulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
--- JLBulkyWeb/Data/ApplicationDbContext.cs
+++ JLBulkyWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using JLBulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace JLBulkyWeb.Data
 {
@@ -6,6 +7,24 @@ namespace JLBulkyWeb.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+        }
+
+        /// <summary>
+        /// Creates a table within DB if it was not created
+        /// </summary>
+        public DbSet<Category> Categories { get; set; }
+
+        /// <summary>
+        /// Use this to seed data into table above
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Sci fi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
         }
     }
 }
